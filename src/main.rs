53ece6f339ce97de864e4576fa93232bfdd76ab7
E-gy <balakhelisey@gmail.com>
2021-05-15 01:50:21 +0200

    hup

--- src/main.rs
+++ src/main.rs
@@ -61,6 +61,7 @@ fn main() -> std::io::Result<()> {
 		log::info!("Constructed paths");
 		serde_json::to_writer(&std::fs::File::create(matches.value_of("output").unwrap())?, &paths).unwrap();
 	} else if let Some(matches) = matches.subcommand_matches("geojson") {
+		log::info!("Loading...");
 		let roads: data::RoadGraphNodes = serde_json::from_reader(&std::fs::File::open(matches.value_of("road-graph").unwrap())?).expect("Road graph config invalid JSON");
 		let paths: data::FlightPaths = serde_json::from_reader(&std::fs::File::open(matches.value_of("paths").unwrap())?).expect("Flight paths invalid JSON");
 		let pref = matches.value_of("prefix").unwrap();
