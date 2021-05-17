d0f7e3430cb4a1988a2e9febd9fa5fa8a4f8ea9d
E-gy <balakhelisey@gmail.com>
2021-05-17 18:15:38 +0200

    wut wut

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,12 @@ mod gj;
 
 pub type NodeId = Cow<'static, str>;
 
+#[derive(serde::Serialize, serde::Deserialize, Clone, PartialEq, Debug)]
+#[serde(untagged)]
+enum Wut {
+	Paths(data::Paths),
+}
+
 fn main() -> std::io::Result<()> {
 	env_logger::init_from_env(env_logger::Env::default().filter_or(env_logger::DEFAULT_FILTER_ENV, "info"));
 	let matches = App::new("Flight Paths Compute")
@@ -45,22 +51,22 @@ fn main() -> std::io::Result<()> {
 										.multiple(true)
 										.help("Let it snow let it snow let it go")))
 							.subcommand(SubCommand::with_name("geojson")
-								.about("Convert computed flight paths into distinct GeoJSONs")
+								.about("Convert anything into GeoJSONs")
 								.arg(Arg::with_name("road-graph")
 										.takes_value(true)
 										.required(true)
 										.index(1)
 										.help("Road Graph JSON"))
-								.arg(Arg::with_name("paths")
+								.arg(Arg::with_name("wut")
 										.takes_value(true)
 										.required(true)
 										.index(2)
-										.help("(Produced) Flight Paths"))
+										.help("Produced thingy that you want to convert (currently supported: flight paths)"))
 								.arg(Arg::with_name("prefix")
 										.takes_value(true)
 										.required(true)
 										.index(3)
-										.help(r#"GeoJSON files prefix - the generated files will be named "{prefix}.{index}.geojson""#))
+										.help(r#"GeoJSON files prefix - the generated files will be named alike "{prefix}.{...}.geojson""#))
 							)
 							.get_matches();
 	log::info!("Loading...");
@@ -81,12 +87,16 @@ fn main() -> std::io::Result<()> {
 		serde_json::to_writer(&std::fs::File::create(matches.value_of("output").unwrap())?, &brr::merge_snow_statuses(snu.into_iter().flatten())).unwrap();
 	} else if let Some(matches) = matches.subcommand_matches("geojson") {
 		let roads: data::RoadGraphNodes = serde_json::from_reader(&std::fs::File::open(matches.value_of("road-graph").unwrap())?).expect("Road graph config invalid JSON");
-		let paths: data::Paths = serde_json::from_reader(&std::fs::File::open(matches.value_of("paths").unwrap())?).expect("Flight paths invalid JSON");
 		let pref = matches.value_of("prefix").unwrap();
+		let wut = serde_json::from_reader(&std::fs::File::open(matches.value_of("wut").unwrap())?).expect("WUT invalid JSON");
 		log::info!("Loaded configuration");
-		let g = gj::roads_to_nodes(roads);
-		for (i, path) in (0..paths.len()).zip(paths.into_iter()) {
-			serde_json::to_writer(&std::fs::File::create(format!("{}.{}.geojson", pref, i))?, &gj::path_to_geojson(&g, path)).unwrap();
+		match wut {
+			Wut::Paths(paths) => {
+				let g = gj::roads_to_nodes(roads);
+				for (i, path) in (0..paths.len()).zip(paths.into_iter()) {
+					serde_json::to_writer(&std::fs::File::create(format!("{}.{}.geojson", pref, i))?, &gj::path_to_geojson(&g, path)).unwrap();
+				}
+			}
 		}
 	}
 	Ok(())
